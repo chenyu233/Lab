46e9b732a46370aabdb8bef6ded9027874023231
chenyu233 <1255282934@qq.com>
2018-06-06 09:30:51 +0800

    lab10

---
+++ lab10/Lab10.php
@@ -0,0 +1,168 @@
+<?php
+//Fill this place
+
+//****** Hint ******
+//connect database and fetch data here
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "travel";
+// 创建连接
+$conn = mysqli_connect($servername, $username, $password,$database);
+// 检测连接
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "连接成功";
+
+function getImages(){
+    global $conn;
+    $sql = "SELECT Title,Path,ImageID,Description,ContinentCode,CountryCodeISO FROM imagedetails";
+    if(isset($_GET["continent"]) && $_GET["continent"] != "" && $_GET['continent'] != "0" ){
+        $continent = $_GET['continent'];
+        $sql .= " WHERE ContinentCode='$continent'";
+        if (isset($_GET["country"]) && $_GET["country"] != ""&& $_GET['country'] != "0"){
+            $country = $_GET['country'];
+            $sql .= " AND CountryCodeISO='$country'";
+        }
+        if (isset($_GET["title"]) && $_GET["title"] != ""){
+            $title = $_GET["title"];
+            $sql .= " AND Title LIKE '%$title%'";
+        }
+    }
+    elseif (isset($_GET["country"]) && $_GET["country"] != ""&& $_GET['country'] != "0"){
+        $country = $_GET['country'];
+        $sql .= " WHERE CountryCodeISO='$country'";
+        if (isset($_GET["title"]) && $_GET["title"] != ""){
+            $title = $_GET["title"];
+            $sql .= " AND Title LIKE '%$title%'";
+        }
+    }
+    elseif (isset($_GET["title"]) && $_GET["title"] != ""){
+        $title = $_GET["title"];
+        $sql .= " WHERE Title LIKE '%$title%'";
+    }
+    $result = mysqli_query($conn, $sql);
+    return $result;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Chapter 14</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/captions.css" />
+    <link rel="stylesheet" href="css/bootstrap-theme.css" />    
+
+</head>
+
+<body>
+    <?php include 'header.inc.php'; ?>
+
+    <!-- Page Content -->
+    <main class="container">
+        <div class="panel panel-default">
+          <div class="panel-heading">Filters</div>
+          <div class="panel-body">
+            <form action="Lab10.php" method="get" class="form-horizontal">
+              <div class="form-inline">
+
+              <select name="continent" class="form-control">
+                <option value="0">Select Continent</option>
+                <?php
+                //Fill this place
+                //****** Hint ******
+                //display the list of continents
+                $sql1 = "SELECT ContinentCode,ContinentName FROM continents ";
+                $result1 = mysqli_query($conn, $sql1);
+                while($row = $result1->fetch_assoc()) {
+                  echo '<option value=' . $row['ContinentCode'] . '>' . $row['ContinentName'] . '</option>';
+                }
+                ?>
+              </select>     
+              
+              <select name="country" class="form-control">
+                <option value="0">Select Country</option>
+                <?php 
+                //Fill this place
+
+                //****** Hint ******
+                /* display list of countries */
+                $sql2 = "SELECT ISO, CountryName FROM countries";
+                $result2 = mysqli_query($conn, $sql2);
+                while($row = $result2->fetch_assoc()) {
+                    echo '<option value=' . $row['ISO'] . '>' . $row['CountryName'] . '</option>';
+                }
+                ?>
+              </select>    
+              <input type="text"  placeholder="Search title" class="form-control" name=title>
+              <button type="submit" class="btn btn-primary">Filter</button>
+              </div>
+            </form>
+
+          </div>
+        </div>     
+                                    
+
+		<ul class="caption-style-2">
+            <?php 
+            //Fill this place
+            $result = getImages();
+             while ($images = $result->fetch_assoc()) {
+                 $href = "detail.php?id=" . $images["ImageID"];
+                 $src = "images/square-medium/" . $images["Path"];
+                 $alt = $images["Title"];
+                 $description = $images["Description"];
+                 echo '<li>';
+                 echo '<a href="' . $href . '" class="img-responsive">';
+                 echo '<img src="' . $src . '" alt="' . $alt . '">';
+                 echo '<div class="caption">';
+                 echo '<div class="blur"></div>';
+                 echo '<div class="caption-text">';
+                 echo '<p>' . '.$description.' . '</p>';
+                 echo '</div>';
+                 echo '</div>';
+                 echo '</a>';
+                 echo '</li>';
+             }
+
+            //****** Hint ******
+            /* use while loop to display images that meet requirements ... sample below ... replace ???? with field data
+            <li>
+              <a href="detail.php?id=????" class="img-responsive">
+                <img src="images/square-medium/????" alt="????">
+                <div class="caption">
+                  <div class="blur"></div>
+                  <div class="caption-text">
+                    <p>????</p>
+                  </div>
+                </div>
+              </a>
+            </li>        
+            */ 
+            ?>
+       </ul>       
+
+      
+    </main>
+    
+    <footer>
+        <div class="container-fluid">
+                    <div class="row final">
+                <p>Copyright &copy; 2017 Creative Commons ShareAlike</p>
+                <p><a href="#">Home</a> / <a href="#">About</a> / <a href="#">Contact</a> / <a href="#">Browse</a></p>
+            </div>            
+        </div>
+    </footer>
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+
+</html>
